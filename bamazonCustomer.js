a3f67643929344892a1a611b31d81b5ebd0227c2
Darrell Abney <dabney@DoCS-1204.local>
2018-04-24 13:25:59 -0400

    prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,4 +38,32 @@ function queryAllItems() {
   });
 }
 
+// The app should then prompt users with two messages.
+
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt({
+      name: "item_id",
+      type: "input",
+      message: "What is the ID of the product they would like to buy?",
+      choices: function() {
+        var choiceArray = [];
+        for (var i = 0; i < res.length; i++) {
+          choiceArray.push(results[i].item_id);
+        }
+        return choiceArray;
+      }
+    })
+}
 
